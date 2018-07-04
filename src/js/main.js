5e79940a35a8c388b5399575b9b59f0a2690c399
Verlevina <levitinava@gmail.com>
2018-07-04 18:25:13 +0300

    главная без футера

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+var experienceText = document.querySelector('.practiceInYears');
+var experience = 0;
+var dateNow = new Date();
+var beginPractice = new Date(2011, 0, 1, 2, 3, 4, 567);
+experience = dateNow.getFullYear() - beginPractice.getFullYear();
+
+function declOfNum(number, titles) {
+  cases = [2, 0, 1, 1, 1, 2];
+  return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+}
+
+var numberTitle = declOfNum(experience, ['год', 'года', 'лет']);
+if (experience) {
+  experienceText.textContent = experience + ' ' + numberTitle;
+} else {
+  experienceText.textContent = 15 + ' лет';
+}
+
